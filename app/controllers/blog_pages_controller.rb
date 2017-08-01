33a6e18daa74ecc66920d0c7f4d8371541434476
yota <pdpaaj@gmail.com>
2017-08-01 11:29:40 +0000

    入力フォーム作成し，スレッドを立てるようにした

--- app/controllers/blog_pages_controller.rb
+++ app/controllers/blog_pages_controller.rb
@@ -6,9 +6,12 @@ class BlogPagesController < ApplicationController
   end
   
   def create
-      puts "Create"
-      #@contribution = User.new(params[:name])
-      #@contribution.save
+      puts "data"
+      puts params[:contribution][:name]
+      @newData = Contribution.new(name: params[:contribution][:name].to_s ,thread_link: " ")
+      @newData.save
+      #TODO:まずDBにURLがいるのかいらないなら消す，saveしないとID取得できないこと
+      @newData.update(thread_link: '/comments/'+ @newData.id.to_s)
       redirect_to :controller => :blog_pages, :action => :home
   end
       
